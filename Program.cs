7c8fca5b6ee339850c90980d51cd22bb35d0c4b9
cesararturo02 <78991542+cesararturo02@users.noreply.github.com>
2021-03-08 23:49:52 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace banco_con_3_clientes
+{
+    class Cliente
+    {
+        private string nombre;
+        private int monto;
+
+        public Cliente(string nom)
+        {
+            nombre = nom;
+            monto = 0;
+        }
+
+        public void Depositar(int m)
+        {
+            monto = monto + m;
+        }
+
+        public void Extraer(int m)
+        {
+            monto = monto - m;
+        }
+
+        public int RetornarMonto()
+        {
+            return monto;
+        }
+
+        public void Imprimir()
+        {
+            Console.WriteLine(nombre + " tiene depositado la suma de " + monto);
+        }
+    }
+
+    class Banco
+    {
+        private Cliente cliente1, cliente2, cliente3;
+
+        public Banco()
+        {
+            cliente1 = new Cliente("miguel");
+            cliente2 = new Cliente("cesar");
+            cliente3 = new Cliente("carla");
+        }
+
+        public void Operar()
+        {
+            cliente1.Depositar(5000);
+            cliente2.Depositar(1500);
+            cliente3.Depositar(2000);
+            cliente3.Extraer(150);
+        }
+
+        public void DepositosTotales()
+        {
+            int t = cliente1.RetornarMonto() +
+                    cliente2.RetornarMonto() +
+                    cliente3.RetornarMonto();
+            Console.WriteLine("El total de dinero en el banco es:" + t);
+            cliente1.Imprimir();
+            cliente2.Imprimir();
+            cliente3.Imprimir();
+        }
+
+        static void Main(string[] args)
+        {
+            Banco banco1 = new Banco();
+            banco1.Operar();
+            banco1.DepositosTotales();
+            Console.ReadKey();
+        }
+    }
+}
